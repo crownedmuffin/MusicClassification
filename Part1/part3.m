8dd3572f3b544e986fc104fc0e3c7e292ed606d6
crownedmuffin <rjsigar@gmail.com>
2016-12-11 01:54:20 -0700

    Migrated working part 3 code from sandbox.m to part3.m
    Former-commit-id: 5347a3d4d4824da598acc5533200624d0358878c

--- Part1/part3.m
+++ Part1/part3.m
@@ -1,120 +1,133 @@
-%{
------------------------Compute the 6 x 6 average distance matrix between the genres-----------------------
-%} 
+numberOfSongs = 150;
+numberOfSongsInFolder = 25;
+numberOfGenres = 6;
+
 
 %NOTE: Matlab must be operating in the folder containing this script for the
 %code below to work. Will need to eliminate this dependency sometime.
 datasetSubfolder = './data/';
 genreSubfolders = dir(datasetSubfolder);
 
-numberOfSongs = 150;
-numberOfGenres = 6;
-
 %Ad-hoc solution to get rid of source controlled remnants detected in the folder
 %These are added as the first contents in the struct
 genreSubfolders(1:2) = []; 
-avgGenreDistance = zeros(numberOfGenres,numberOfGenres);
-songDistances = zeros(numberOfSongs,numberOfSongs);
 
+%Amount of seconds in two minutes
 twoMinutes = 2*60;
 
- %frame size shall be 512 as described in project
+%frame size shall be 512 as described in project
 frameSize_N = 512;
 window = hann(frameSize_N);
 
-songCount = 1;
+%Switch indicating that we need to compute mfcc values 
+need_to_compute_mfcc = 1;
+
+
+song = struct('filepath',[]);
+for i = 1:numberOfGenres
+    %Get path to genre subfolder
+    path_to_genre_subfolders = [datasetSubfolder genreSubfolders(i).name '/'];
+    songs_in_subfolder = dir(path_to_genre_subfolders);
+    
+    %Get rid of source controlled remnants (usuallyy first two files listed)
+    songs_in_subfolder(1:2) = [];
+    
+    for j = 1:25
+       
+        %Create an index for each song. 
+        songIndex = ((i-1)*25) + j;
+        
+         %Catalog song path in a struct
+        song(songIndex).filepath = [path_to_genre_subfolders songs_in_subfolder(j).name];
+    end
+end
+
+precomputed_mfcc_values = struct('result',[]);
+
+try
+    load('precomputed_mfcc_values.mat')
+    need_to_compute_mfcc = 0;
+catch
+    warning('File: precomputed_mfcc_values.mat not present.')
+    need_to_compute_mfcc = 1;
+end
+
+
+%Compute mfcc values if we don't have pre-loaded values. Should take about 1.5 minutes on a dual core laptop.
+if need_to_compute_mfcc
+    tic
+    for i = 1:numberOfGenres
+        for j = 1:numberOfSongsInFolder
+           [song_signal, song_sampleRate] = slice_audio(song(songIndex).filepath,twoMinutes,1); 
+
+
+           songIndex = ((i-1)*25) + j;
+
+           mfcc_result = mfcc(song_signal,song_sampleRate,frameSize_N,window);
+           precomputed_mfcc_values(songIndex).result = mfcc_result;
+        end
+    end
+    toc
+end
+
+%Average distance across genres
+avgGenreDistance = zeros(numberOfGenres,numberOfGenres);
+
+%Matrix containing the distance each song is from each other
+songDistances = zeros(numberOfSongs,numberOfSongs);
+
+currentCompare = [0 0 0 0];
+prevCompare = [0 0 0 0];
+
+
+%Computing distance(songX,songY)
 
 tic
-for genre1 = 1:6
-    
-    %Get names of all songs in genre 1
-    path_to_genre1_songs = [datasetSubfolder genreSubfolders(genre1).name '/'];
-    genre1_songs = dir(path_to_genre1_songs);
-    
-    %Ad-hoc solution to get rid of source controlled remnants detected in the folder
-    genre1_songs(1:2) = []; 
-    
-    for genre1_song = 1:25
-        %Load genre songs
-        song1_filepath = [path_to_genre1_songs genre1_songs(genre1_song).name];
-        [song1_signal, song1_sampleRate] = slice_audio(song1_filepath,twoMinutes,1);
-
-        mfcc1 = mfcc(song1_signal,song1_sampleRate,frameSize_N,window);
-
-        %Only perform computations on left side of distance matrix for
-        %efficiency.
-        for genre2 = genre1:6
-            %{
-            
-            %Skip computation over similar genres
-            
-            if genre1 == genre2
-                %For computing average distance matrix
-                %If same genre, distance is 0
-                avgGenreDistance(genre1,genre2) = 0;
-                %Move to next genre
-                continue;
+for genreX = 1:6
+    for songX = 1:25
+        songXIndexOffset = ((genreX-1)*25);
+        songXIndex = songXIndexOffset + songX;
+        for genreY = genreX:6
+            for songY = 1:25
+                %Computing index (song number) for songY
+                songYIndexOffset = ((genreY-1)*25);
+                songYIndex = songYIndexOffset + songY;
+                
+                %fprintf('genreX = %d, songX = %d, genreY = %d, songY = %d\n',genreX,songX, genreY, songY)
+                %fprintf('distance(%d,%d)\n\n',songXIndex,songYIndex)
+                try
+                 songDistances(songXIndex,songYIndex) = distanceBetweenSongs(precomputed_mfcc_values(songXIndex).result, ...
+                                                                             precomputed_mfcc_values(songYIndex).result);
+                 %Map values to other side of diagonol for a completed
+                 %matrix
+                 songDistances(songYIndex,songXIndex) = songDistances(songXIndex,songYIndex);
+                catch
+                    warning('Something is wrong.')
+                    fprintf('Trouble computing songX = %d, songY = %d',songXIndex,songYIndex);
+                end
+                
+                avgGenreDistance(genreX,genreY) = songDistances(songXIndex,songYIndex)...
+                                                  + avgGenreDistance(genreX,genreY);
+                                              
+                avgGenreDistance(genreY,genreX) = avgGenreDistance(genreX,genreY);
+               
             end
-            %}
-            
-            
-            %Get names of all songs in genre2
-            path_to_genre2_songs = [datasetSubfolder genreSubfolders(genre2).name '/'];
-            genre2_songs = dir(path_to_genre2_songs);
-
-            %Ad-hoc solution to get rid of source controlled remnants detected in the folder
-            genre2_songs(1:2) = []; 
-
-            for genre2_song = 1:25
-                
-                songCount = songCount + 1
-
-                %Read audio file
-                song2_filepath = [path_to_genre2_songs genre2_songs(genre2_song).name];
-                [song2_signal, song2_sampleRate] = slice_audio(song2_filepath,twoMinutes,1); 
-
-                %Compute mfcc coefficients for genre2's song
-                mfcc2 = mfcc(song2_signal,song2_sampleRate,frameSize_N,window);
-
-                %Capture individual song distance data.
-                
-                %NOTE: 
-                %Compute data under left side of diagonol first, then copy
-                %values over to other side.
-                
-                %Assigning a 'song index' to each song (1 through 150).
-                song1Index = genre1*genre1_song;
-                song2Index = genre2*genre2_song;
-                
-                %Calculating distance between songs
-                songDistance(song1Index,song2Index) = distanceBetweenSongs(mfcc1,mfcc2);
-                songDistances(song2Index,song1Index) = songDistances(song1Index,song2Index);
-                
-                %EQUATION 26 (part 1 of 2): 
-                %Iteratively add distances between genres below.
-                
-                %tic and toc were used to time this function
-                %tic
-                    avgGenreDistance(genre1,genre2) = (songDistances(song1Index,song2Index)...
-                                                + avgGenreDistance(genre1,genre2));
-                %toc      
-            end
-            
-            %EQUATION 26 (part 2 of 2): 
-            %Finally, to obtain the average genre distance, we divide by
-            %the number of data points, (1/25)^2, used to compute the total distance from
-            %the above for loop.
-            avgGenreDistance(genre1,genre2) = (1/(25^2))*avgGenreDistance(genre1,genre2);
-            
-            %Matrix is symmetric. After computing half of the values on left side of the diagonol, 
-            %copy values to other half of diagonol on the 6x6 avg distance matrix.
-            avgGenreDistance(genre2,genre1) = avgGenreDistance(genre1,genre2);
         end
      end
 end  
 toc
 
-imagesc(avgGenreDistance);
-imagesc(songDistances);
-colorbar;
-colormap('jet');
+%Compute the average distance: divide by number of data points used to 
+%compute the total distances (625)
+avgGenreDistance = (1/(25^2))*avgGenreDistance;
+
+
+figure
+fig1 = imagesc(songDistances);
+colormap('jet')
+colorbar
+
+figure
+fig2 = imagesc(avgGenreDistance);
+colormap('jet')
+colorbar
